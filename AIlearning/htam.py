61544937b7172741ab171a695584b201ca0fe9f9
Asteroid <1158140460@qq.com>
2025-03-17 00:12:23 +0800

    s1 1h18min40sed

--- AIlearning/htam.py
+++ AIlearning/htam.py
@@ -19,3 +19,36 @@ print(f'{g.data:.4f}') # prints 24.7041, the outcome of this forward pass
 g.backward()
 print(f'{a.grad:.4f}') # prints 138.8338, i.e. the numerical value of dg/da
 print(f'{b.grad:.4f}') # prints 645.5773, i.e. the numerical value of dg/db
+
+# 定义 x 轴的值
+x = np.linspace(-2 * np.pi, 2 * np.pi, 400)
+
+# 计算 tan 和 tanh 的值
+tan_values = np.tan(x)
+tanh_values = np.tanh(x)
+
+# 创建一个新的图形
+plt.figure(figsize=(12, 6))
+
+# 绘制 tan 函数
+plt.subplot(1, 2, 1)
+plt.plot(x, tan_values, label='tan(x)')
+plt.ylim(-10, 10)  # 限制 y 轴的范围以更好地显示 tan 函数
+plt.title('tan(x)')
+plt.xlabel('x')
+plt.ylabel('tan(x)')
+plt.legend()
+plt.grid(True)
+
+# 绘制 tanh 函数
+plt.subplot(1, 2, 2)
+plt.plot(x, tanh_values, label='tanh(x)', color='orange')
+plt.title('tanh(x)')
+plt.xlabel('x')
+plt.ylabel('tanh(x)')
+plt.legend()
+plt.grid(True)
+
+# 显示图形
+plt.tight_layout()
+plt.show()
